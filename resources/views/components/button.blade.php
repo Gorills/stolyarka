1ae9635a33c17e281f6e9fcb459f60e29d32fbb7
Gorills <gorivanickiy@gmail.com>
2020-09-14 11:39:32 +0700

    tel whats

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,11 +1,11 @@
 <div class="phone-btn">
-    <a href="#" class="phone-btn__link show_popup">
+    <a href="tel:89832396889" class="phone-btn__link">
         <i class="phone-btn__icon fas fa-phone"></i>
     </a>
 </div>
 
 <div class="whatsapp-btn">
-    <a href="#" class="whatsapp-btn__link show_popup2">
+    <a href="https://wa.me/79832396889" class="whatsapp-btn__link">
         <i class="whatsapp-btn__icon fab fa-whatsapp"></i>
     </a>
 </div>
